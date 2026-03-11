6a1389aa1553a3fff90dcd981c9814a757351988
Shota Hoshino <sht.hshn@gmail.com>
2026-03-11 16:19:56 +0900

    fix: return Either from MacKey byte-array constructors

--- projects/core/src/main/scala/crypto4s/MacKey.scala
+++ projects/core/src/main/scala/crypto4s/MacKey.scala
@@ -12,10 +12,14 @@ trait MacKey[Alg] {
 
 object MacKey {
   def hmacSHA1(size: Int = 160): MacKey[HmacSHA1]    = generate("HmacSHA1", size)
-  def hmacSHA1(key: Array[Byte]): MacKey[HmacSHA1]   = JavaMacKey(new SecretKeySpec(key, "HmacSHA1"))
+  def hmacSHA1(key: Array[Byte]): Either[IllegalArgumentException, MacKey[HmacSHA1]] =
+    try Right(JavaMacKey(new SecretKeySpec(key, "HmacSHA1")))
+    catch { case e: IllegalArgumentException => Left(e) }
 
-  def hmacSHA256(size: Int = 256): MacKey[HmacSHA256]  = generate("HmacSHA256", size)
-  def hmacSHA256(key: Array[Byte]): MacKey[HmacSHA256] = JavaMacKey(new SecretKeySpec(key, "HmacSHA256"))
+  def hmacSHA256(size: Int = 256): MacKey[HmacSHA256] = generate("HmacSHA256", size)
+  def hmacSHA256(key: Array[Byte]): Either[IllegalArgumentException, MacKey[HmacSHA256]] =
+    try Right(JavaMacKey(new SecretKeySpec(key, "HmacSHA256")))
+    catch { case e: IllegalArgumentException => Left(e) }
 
   private def generate[Alg](algorithm: String, size: Int): MacKey[Alg] = {
     val keyGen = KeyGenerator.getInstance(algorithm)
--- projects/core/src/test/scala/crypto4s/MacingSpec.scala
+++ projects/core/src/test/scala/crypto4s/MacingSpec.scala
@@ -3,6 +3,7 @@ package crypto4s
 import crypto4s.algorithm.HmacSHA1
 import crypto4s.algorithm.HmacSHA256
 import zio.Scope
+import zio.ZIO
 import zio.test.Gen
 import zio.test.Spec
 import zio.test.TestEnvironment
@@ -13,10 +14,12 @@ import zio.test.checkAll
 object MacingSpec extends ZIOSpecDefault {
   private val genKey = Gen.vectorOfBounded(16, 64)(Gen.byte).map(_.toArray)
 
+  private val genHmacSHA1Key   = genKey.mapZIO(key => ZIO.fromEither(MacKey.hmacSHA1(key)).orDie)
+  private val genHmacSHA256Key = genKey.mapZIO(key => ZIO.fromEither(MacKey.hmacSHA256(key)).orDie)
+
   override def spec: Spec[TestEnvironment & Scope, Any] = suiteAll("Macing") {
     test("HmacSHA1: same key and data produce verifiable MAC") {
-      checkAll(Gen.string, genKey) { (string, keyBytes) =>
-        val key  = MacKey.hmacSHA1(keyBytes)
+      checkAll(Gen.string, genHmacSHA1Key) { (string, key) =>
         val mac1 = string.maced[HmacSHA1](key)
         val mac2 = string.maced[HmacSHA1](key)
 
@@ -28,8 +31,7 @@ object MacingSpec extends ZIOSpecDefault {
       }
     }
     test("HmacSHA1: different data produce different MACs") {
-      checkAll(Gen.string, genKey) { (string, keyBytes) =>
-        val key  = MacKey.hmacSHA1(keyBytes)
+      checkAll(Gen.string, genHmacSHA1Key) { (string, key) =>
         val mac1 = string.maced[HmacSHA1](key)
         val mac2 = (string + "a").maced[HmacSHA1](key)
 
@@ -41,20 +43,19 @@ object MacingSpec extends ZIOSpecDefault {
     }
     test("HmacSHA1: different keys produce different MACs") {
       val genTwoKeys = for {
-        key1 <- genKey
-        key2 <- genKey if !java.util.Arrays.equals(key1, key2)
+        key1 <- genHmacSHA1Key
+        key2 <- genHmacSHA1Key if !java.util.Arrays.equals(key1.asJava.getEncoded, key2.asJava.getEncoded)
       } yield (key1, key2)
 
-      checkAll(Gen.string, genTwoKeys) { case (string, (keyBytes1, keyBytes2)) =>
-        val mac1 = string.maced[HmacSHA1](MacKey.hmacSHA1(keyBytes1))
-        val mac2 = string.maced[HmacSHA1](MacKey.hmacSHA1(keyBytes2))
+      checkAll(Gen.string, genTwoKeys) { case (string, (key1, key2)) =>
+        val mac1 = string.maced[HmacSHA1](key1)
+        val mac2 = string.maced[HmacSHA1](key2)
 
         assertTrue(!mac1.verify(mac2))
       }
     }
     test("HmacSHA256: same key and data produce verifiable MAC") {
-      checkAll(Gen.string, genKey) { (string, keyBytes) =>
-        val key  = MacKey.hmacSHA256(keyBytes)
+      checkAll(Gen.string, genHmacSHA256Key) { (string, key) =>
         val mac1 = string.maced[HmacSHA256](key)
         val mac2 = string.maced[HmacSHA256](key)
 
@@ -66,8 +67,7 @@ object MacingSpec extends ZIOSpecDefault {
       }
     }
     test("HmacSHA256: different data produce different MACs") {
-      checkAll(Gen.string, genKey) { (string, keyBytes) =>
-        val key  = MacKey.hmacSHA256(keyBytes)
+      checkAll(Gen.string, genHmacSHA256Key) { (string, key) =>
         val mac1 = string.maced[HmacSHA256](key)
         val mac2 = (string + "a").maced[HmacSHA256](key)
 
@@ -78,8 +78,7 @@ object MacingSpec extends ZIOSpecDefault {
       }
     }
     test("HmacSHA256: Blob conversion") {
-      checkAll(Gen.string, genKey) { (string, keyBytes) =>
-        val key    = MacKey.hmacSHA256(keyBytes)
+      checkAll(Gen.string, genHmacSHA256Key) { (string, key) =>
         val maced  = string.maced[HmacSHA256](key)
         val asBlob = maced.blob
         val asHex  = maced.asHexString
@@ -109,7 +108,7 @@ object MacingSpec extends ZIOSpecDefault {
       )
     }
     test("MacKey.hmacSHA256(size) generates a key with specified size") {
-      val key = MacKey.hmacSHA256(512)
+      val key   = MacKey.hmacSHA256(512)
       val maced = "hello".maced[HmacSHA256](key)
 
       assertTrue(
@@ -126,14 +125,19 @@ object MacingSpec extends ZIOSpecDefault {
 
       assertTrue(!mac1.verify(mac2))
     }
+    test("MacKey.hmacSHA256(key) returns Left for empty key") {
+      assertTrue(MacKey.hmacSHA256(Array.empty[Byte]).isLeft)
+    }
+    test("MacKey.hmacSHA1(key) returns Left for empty key") {
+      assertTrue(MacKey.hmacSHA1(Array.empty[Byte]).isLeft)
+    }
     test("equalities") {
       val strings = for {
         str1 <- Gen.string
         str2 <- Gen.string if str1 != str2
       } yield (str1, str2)
 
-      checkAll(strings, genKey) { case ((str1, str2), keyBytes) =>
-        val key  = MacKey.hmacSHA256(keyBytes)
+      checkAll(strings, genHmacSHA256Key) { case ((str1, str2), key) =>
         val mac1 = str1.maced[HmacSHA256](key)
         val mac2 = str1.maced[HmacSHA256](key)
         val mac3 = str2.maced[HmacSHA256](key)
